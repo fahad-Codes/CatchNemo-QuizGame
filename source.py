1a92f31e9e177cef3aa11a155bf9d29388e2e0b1
Muhammad Fahad Shafique <111996171+fahad-Codes@users.noreply.github.com>
2023-08-08 15:37:28 +0500

    Catch Nemo game load functionaluty + Data File

--- source.py
+++ source.py
@@ -1,18 +1,22 @@
+import os
+
+
 class Node:
     def __init__(self, data=" "):
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-    
+
     def is_null(self):
         if self.head is None:
             return " "
         return "x"
-    
+
     def insert(self, d):
         temp = Node(d)
         if self.head is None:
@@ -21,7 +25,7 @@ class LinkedList:
         else:
             self.tail.next = temp
             self.tail = temp
-    
+
     def delete_node(self):
         if self.head is None:
             return " "
@@ -30,36 +34,36 @@ class LinkedList:
         self.head = temp.next
         del temp
         return d
-    
+
     def display_ll(self):
         temp = self.head
         while temp is not None:
             print(temp.data, "--->", end="")
             temp = temp.next
         print("nullptr")
-        
+
 
 class Queue:
     def __init__(self):
         self.data = LinkedList()
         self.rare = 0
         self.front = 0
-    
+
     def is_empty(self):
         if self.front == self.rare:
             return True
         return False
-    
+
     def is_null(self):
         if self.data.is_null() == " ":
-            return " "
+            return True
         else:
-            return "x"
-    
+            return False
+
     def enqueue(self, d):
         self.data.insert(d)
         self.rare += 1
-    
+
     def dequeue(self):
         if not self.is_empty():
             if self.front == 0:
@@ -72,24 +76,86 @@ class Queue:
         else:
             print("The queue is empty.")
             return " "
-    
+
+    def empty(self):
+        while not self.is_empty():
+            x = self.dequeue()
+
     def display(self):
         while not self.is_empty():
             x = self.dequeue()
             print("Data:", x)
 
+
 class CatchNemo:
-    Question = Queue
-    Answer = Queue
     Nemo = int
-    
+    Questions = Queue()
+    Answers = Queue()
+
     def __init__(self):
-        Question = None
-        Answer = None
-        Nemo = 0 
-    
-    def LoadGames(self, filename):
-        
-    
-        
-        
+        self.Nemo = 0
+
+    def LoadGames(self, filename, Stagename):
+        os.system("clear")
+        self.Questions.empty()
+        self.Answers.empty()
+        print("Current Nemo(s):", self.Nemo)
+        with open(filename, "r") as fin:  # OPENING FILE IN READ MODE
+            if not fin:
+                print(
+                    "\n\n Stage 1 questions are not stored, Press any key to continue...!!")
+                input()
+                exit()
+
+            print("\n\t\t\t <---------------------Catch The Nemo---------------------> ")
+            print("\n\t\t  	                 <---" + Stagename + "--->")
+
+            for line in fin:  # WHILE LOOP TO RUN UNTIL THE END OF FILE
+                line = line.strip()  # Removing leading/trailing whitespace
+                if not line:  # ENDS LOOP WHEN EMPTY LINE IS FOUND
+                    break
+
+                # SEPARATING LINE BASED ON DELIMITER (COMMA)
+                str_arr = line.split(';')
+                self.Questions.enqueue(str_arr[0])  # TURNING STRING TO INTEGER
+                self.Answers.enqueue(str_arr[1])
+        while not self.Questions.is_empty():
+            print("\n\t\t", self.Questions.dequeue(),
+                  "\n\t\t Enter your Answer here: ")
+            line = input()
+            line = line.upper()  # Transform the string to uppercase
+            if line != self.Answers.dequeue():
+                print("\n\t\tWrong Answer......!!!!!! Press any key to start Over")
+                flag = 0
+                if self.Nemo > 0:
+                    choice = input(
+                        "\n\tOr You can lose 1 nemo to skip this question. (y/n): ")
+                    choice = choice.lower()
+                    while flag == 0:
+                        if choice == 'y':
+                            self.Nemo -= 1
+                            flag = 1
+                        elif choice == 'n':
+                            self.LoadGames(filename)
+                            flag = 1
+                        else:
+                            print(
+                                "\tYou selected the wrong choice, Please select from y/n:")
+                            choice = input().lower()
+                if flag == 0:
+                    input()
+                    self.LoadGames(filename)
+
+        self.Nemo += 1
+        print("\n\t\t\tCongrats!! You cleared the stage 1 of the Easy level.")
+        print("\n\t\t\t You caught your First Nemo, hurrah!!")
+        print("\n\t\t\t Now you have", self.Nemo, "Nemos in your bag.")
+        input()
+
+        with open("records.txt", "w") as fout:  # OPENING FILE IN WRITE MODE
+            fout.write("1;2;" + str(self.Nemo))
+            exit(self.Nemo)
+
+
+game = CatchNemo()
+game.LoadGames("EasyStage1.txt", "Easy level Stage 1")
